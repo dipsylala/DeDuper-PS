9394a3f20dcdae43c019c91e9ed63d9eb6ef0e6c
dipsylala <github@cus-productions.com>
2022-06-22 21:31:59 +0100

    Create DeDuper.ps1

---
+++ DeDuper.ps1
@@ -0,0 +1,16 @@
+$Source = 'c:\input'
+$Destination = 'c:\output'
+$Filter = '*.dll;*.pdb'
+
+foreach ($File in Get-ChildItem -File $Source -Recurse) {
+    $FileBaseName = $File.BaseName
+    $Hash = (Get-FileHash $File -Algorithm MD5).Hash
+
+    $FullDestinationDirectory = Join-Path $Destination $Hash.substring(0, 2) ($File.BaseName + ' - ' + $Hash)
+
+    if(!(Test-Path $FullDestinationDirectory)){
+        New-Item -ItemType "directory" -Path $FullDestinationDirectory | Out-Null
+    }
+
+    Copy-Item -Path $File.FullName -Destination $FullDestinationDirectory\$FileBaseName
+}
