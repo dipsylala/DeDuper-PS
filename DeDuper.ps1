70088ab2d37948c2fba96fd284927810bafba83e
dipsylala <github@cus-productions.com>
2022-06-22 21:57:21 +0100

    Filter works, full filename copied

--- DeDuper.ps1
+++ DeDuper.ps1
@@ -1,12 +1,12 @@
 $Source = 'c:\input'
 $Destination = 'c:\output'
-$Filter = '*.dll;*.pdb'
+$Filter = ('*.dll', '*.pdb')
 
-foreach ($File in Get-ChildItem -File $Source -Recurse) {
-    $FileBaseName = $File.BaseName
+foreach ($File in Get-ChildItem -File $Source -Recurse -Include $Filter) {
+    $FileBaseName = $File.Name
     $Hash = (Get-FileHash $File -Algorithm MD5).Hash.ToLower()
 
-    $FullDestinationDirectory = Join-Path $Destination $Hash.substring(0, 2) ($File.BaseName + ' - ' + $Hash)
+    $FullDestinationDirectory = Join-Path $Destination $Hash.substring(0, 2) ($File.Name + ' - ' + $Hash)
 
     if(!(Test-Path $FullDestinationDirectory)){
         New-Item -ItemType "directory" -Path $FullDestinationDirectory | Out-Null
